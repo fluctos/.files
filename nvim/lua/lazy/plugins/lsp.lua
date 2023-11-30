f3e78929265a6d548e353090f56c249840f4f5e9
Fluctos <152332422+fluctos@users.noreply.github.com>
2023-11-30 18:20:13 +0100

    feat: basic lsp configuration

--- nvim/lua/lazy/plugins/lsp.lua
+++ nvim/lua/lazy/plugins/lsp.lua
@@ -1,24 +1,46 @@
+local on_attach = function()
+    vim.o.signcolumn = 'yes:1'
+    local telescope = require('telescope.builtin')
+    vim.keymap.set('n', 'gd', telescope.lsp_definitions)
+    vim.keymap.set('n', 'gt', telescope.lsp_type_definitions)
+    vim.keymap.set('n', 'gi', telescope.lsp_implementations)
+    vim.keymap.set('n', 'gr', telescope.lsp_references)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+    vim.keymap.set('n', '<Leader>cr', vim.lsp.buf.rename)
+    vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action)
+end
+
 return {
     {
-        'neovim/nvim-lspconfig',
-    },
-    {
         'williamboman/mason.nvim',
-        dependencies = 'nvim-lspconfig',
-        config = function()
-            require('mason').setup({
-                ui = {border = 'none'},
-            })
-        end,
+        config = true,
     },
     {
         'williamboman/mason-lspconfig.nvim',
-        dependencies = {
-            'mason.nvim',
+        config = {
+            ensure_installed = {
+                'clangd',
+                'lua_ls',
+            },
         },
+
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = 'nvim-telescope/telescope.nvim',
         config = function()
-            require('mason-lspconfig').setup({
-                ensure_installed = { 'lua_ls', },
+            require('lspconfig').lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = {'vim'},
+                        },
+                    },
+                },
+                on_attach = on_attach
+            })
+            require('lspconfig').clangd.setup({
+                on_attach = on_attach
             })
         end,
     },
