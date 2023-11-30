f48fa1e8de5b57372dddc91ba5537a3b32aad07f
Fluctos <152332422+fluctos@users.noreply.github.com>
2023-11-30 15:22:36 +0100

    feat: better keymaps for indenting and searching

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,6 +1,11 @@
-
 vim.keymap.set('n', '<C-h>', '<C-W>h')
 vim.keymap.set('n', '<C-j>', '<C-W>j')
 vim.keymap.set('n', '<C-k>', '<C-W>k')
 vim.keymap.set('n', '<C-l>', '<C-W>l')
+
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
 
