e96e43654d0cd35cc5a7f0055f714f72285360e9
Fluctos <152332422+fluctos@users.noreply.github.com>
2023-12-03 02:10:27 +0100

    feat: lsp configuration for rust_analyzer

--- nvim/lua/lazy/plugins/lsp.lua
+++ nvim/lua/lazy/plugins/lsp.lua
@@ -1,46 +1,66 @@
-local on_attach = function()
-    vim.o.signcolumn = 'yes:1'
-    local telescope = require('telescope.builtin')
-    vim.keymap.set('n', 'gd', telescope.lsp_definitions)
-    vim.keymap.set('n', 'gt', telescope.lsp_type_definitions)
-    vim.keymap.set('n', 'gi', telescope.lsp_implementations)
-    vim.keymap.set('n', 'gr', telescope.lsp_references)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover)
-    vim.keymap.set('n', '<Leader>cr', vim.lsp.buf.rename)
-    vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action)
-end
-
 return {
     {
-        'williamboman/mason.nvim',
-        config = true,
-    },
-    {
-        'williamboman/mason-lspconfig.nvim',
-        config = {
-            ensure_installed = {
-                'clangd',
-                'lua_ls',
-            },
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
         },
 
-    },
-    {
-        'neovim/nvim-lspconfig',
-        dependencies = 'nvim-telescope/telescope.nvim',
         config = function()
-            require('lspconfig').lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = {'vim'},
+            local servers = {
+                clangd = {},
+
+                rust_analyzer = {
+                    cargo = {
+                      allFeatures = true,
+                      loadOutDirsFromCheck = true,
+                      runBuildScripts = true,
+                    },
+                    diagnostics = {
+                        enable = true,
+                        experimental = {
+                            enable = true,
                         },
                     },
                 },
-                on_attach = on_attach
+
+                lua_ls = {
+                    Lua = {
+                        diagnostics = {globals = {'vim'}},
+                        workspace = {checkThirdParty = false},
+                        telementry = {enable = false},
+                    },
+                }
+            }
+
+            local on_attach = function()
+                vim.o.signcolumn = 'yes:1'
+                local telescope = require('telescope.builtin')
+                vim.keymap.set('n', 'gd', telescope.lsp_definitions)
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+                vim.keymap.set('n', 'gt', telescope.lsp_type_definitions)
+                vim.keymap.set('n', 'gi', telescope.lsp_implementations)
+                vim.keymap.set('n', 'gr', telescope.lsp_references)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+                vim.keymap.set('n', '<Leader>cr', vim.lsp.buf.rename)
+                vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action)
+            end
+
+            require('mason').setup()
+            require('mason-lspconfig').setup({
+                ensure_installed = vim.tbl_keys(servers)
             })
-            require('lspconfig').clangd.setup({
-                on_attach = on_attach
+
+            require('mason-lspconfig').setup_handlers({
+                function(server_name)
+                    require('lspconfig')[server_name].setup({
+                        capabilities = vim.lsp.protocol.make_client_capabilities(),
+                        on_attach = on_attach,
+                        settings = servers[server_name],
+                        filetypes = (servers[server_name] or {}).filetypes,
+                    })
+                end,
             })
         end,
     },
