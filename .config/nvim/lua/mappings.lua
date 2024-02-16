e3370daf57e5e7d2b78970e818e2fbfdb51214c0
Fluctos <152332422+fluctos@users.noreply.github.com>
2024-02-16 11:00:39 +0100

    feat: mappings for quicklist

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -14,3 +14,8 @@ vim.keymap.set('n', '<Leader>ww', '<cmd>write<Cr>')
 vim.keymap.set('n', '[t', '<cmd>tabprev<Cr>')
 vim.keymap.set('n', ']t', '<cmd>tabnext<Cr>')
 
+vim.keymap.set('n', '[q', '<cmd>cp<Cr>zz')
+vim.keymap.set('n', ']q', '<cmd>cn<Cr>zz')
+
+vim.keymap.set('n', '<C-i>', '<C-i>zz')
+vim.keymap.set('n', '<C-o>', '<C-o>zz')
