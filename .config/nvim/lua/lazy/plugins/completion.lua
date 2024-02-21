5993ba24195355f6664dc3f1b7cbcb78370a3099
Fluctos <152332422+fluctos@users.noreply.github.com>
2024-02-21 17:46:16 +0100

    refactor: tab to confirm completion, snippets removed for now

--- .config/nvim/lua/lazy/plugins/completion.lua
+++ .config/nvim/lua/lazy/plugins/completion.lua
@@ -1,3 +1,4 @@
+
 return {
     {
         'hrsh7th/nvim-cmp',
@@ -5,57 +6,41 @@ return {
         dependencies = {
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-path',
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
         },
 
         config = function()
             local cmp = require('cmp')
-            local luasnip = require('luasnip')
-
-            luasnip.config.setup()
 
             cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end
-                },
+
                 completion = {
-                    completeopt = 'menu,menuone,noinsert',
+                    completeopt = 'menu,menuone,noselect',
                 },
-                mapping = cmp.mapping.preset.insert {
-                    ['<C-n>'] = cmp.mapping.select_next_item(),
-                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-j>'] = cmp.mapping.select_next_item(),
+                    ['<C-k>'] = cmp.mapping.select_prev_item(),
+
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete {},
-                    ['<CR>'] = cmp.mapping.confirm {
-                        behavior = cmp.ConfirmBehavior.Replace,
-                        select = true,
-                    },
-                    ['<Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_locally_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
-                    ['<S-Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
-                },
+
+                    ['<Tab>'] = cmp.mapping(
+                        function(fallback)
+                            if cmp.visible() then
+                                cmp.confirm({
+                                    behavior = cmp.ConfirmBehavior.Insert,
+                                    select = true,
+                                })
+                            else
+                                fallback()
+                            end
+                        end,
+                        {'i', 's'}
+                    ),
+                }),
+
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
                     { name = 'path' },
                 }),
 
